a48952b7edfe48eb8bf7a59049aa63b6d3d2b5f0
Candice Cirbo <thecirbos@icloud.com>
2024-04-27 15:47:36 -0600

    Mild code challege redone.

--- code_challenges/cfu_challenge1.rb
+++ code_challenges/cfu_challenge1.rb
@@ -7,32 +7,15 @@
 # create a new Ruby file inside this `code_challenges` directory and 
 # choose a name that makes sense.
 
-# ## Mild
 
-#even_or_odd method takes in a number argument and returns a string even or odd.
-
-
-# So this method returns strings "even" or "odd".
-def even_or_odd(num)
-    # 2.  "odd" if the Integer is odd.
-    if num.odd?
-        p "This number is odd." 
-    # 3. "even" if the Integer is even,
-    else
-        p "This number is even."
-    end
-end
-
-even_or_odd(3)
-even_or_odd(8)
-
-p ("*"* 20)
 # ## Mild
 
 # Write a Ruby program that defines a variable that stores an Integer. 
 # The program should print out the String "even" if the Integer is even,
 # and the String "odd" if the Integer is odd.
-num = 8
+
+# So this method returns strings "even" or "odd".
+num = 97
 if num.odd?
     p "This number is odd." 
 else
